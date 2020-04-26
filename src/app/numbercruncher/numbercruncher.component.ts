65eab99c99705837a61357883e49675b9d260f3a
Grady Peck <gradypeck717@gmail.com>
2020-04-26 06:30:43 -0400

    finished first pass of CSV parse, extracting ingredients tomorrow

--- src/app/numbercruncher/numbercruncher.component.ts
+++ src/app/numbercruncher/numbercruncher.component.ts
@@ -3,6 +3,13 @@ import { Component, OnInit, Input } from '@angular/core';
 import { NgxCsvParser } from 'ngx-csv-parser';
 import { NgxCSVParserError } from 'ngx-csv-parser';
 
+interface IRecipe {
+  name : string;
+  ingredients : string;
+  products : string;
+  data : string;
+}
+
 @Component({
   selector: 'app-numbercruncher',
   templateUrl: './numbercruncher.component.html',
@@ -12,15 +19,11 @@ import { NgxCSVParserError } from 'ngx-csv-parser';
 export class NumbercruncherComponent implements OnInit {
 
   public form: {
-		name: string;
-		email: string;
 		resume: File | null;
 	};
 
   constructor(private csvparser : NgxCsvParser) { 
     this.form = {
-			name: "",
-			email: "",
 			resume: null
 		};
   }
@@ -28,9 +31,9 @@ export class NumbercruncherComponent implements OnInit {
   ngOnInit() {
   }
 
-  honk : FileList;
-
   csvRecords = [];
+
+  recipes : IRecipe[] = [];
 
   parsley (inpoot : File) {
     this.csvparser.parse(inpoot, { header: false, delimiter: '@' })
@@ -39,11 +42,7 @@ export class NumbercruncherComponent implements OnInit {
         //console.log('Result', result);
         this.csvRecords = result;
         for (let row of this.csvRecords) {
-          // let newRow : string[] = this.fixRow(row);
-           for (let item of row) {
-             console.log(item);
-           }
-          console.log(row);
+           this.recipes.push({'name' : row[0], 'ingredients' : row[1], 'products' : row[2], 'data' : row[3]});
         }
       }, (error: NgxCSVParserError) => {
         console.log('Error', error);
@@ -52,30 +51,7 @@ export class NumbercruncherComponent implements OnInit {
     }
 
     public processForm() : void {
- 
-      console.group( "Form View-Model" );
-      console.log( "Name:", this.form.name );
-      console.log( "Email:", this.form.email );
-      console.log( "Resume:", this.form.resume);
-      console.groupEnd();
-      console.log(this.form.resume);
       this.parsley(this.form.resume);
-    }
-
-    public fixRow (row : string[]) : string[] {
-      if(row.length < 4) {
-        row[0] = row[0].slice(0, row[0].indexOf("No Ingredients")-1);
-      }
-      else {
-        for (let i = 0; i < 3; i++) {
-          row[i] = row[i].slice(0, row[i].length - 2);
-        }
-        for (let i = 1; i < 4; i++) {
-          row[i] = "{" + row[i];
-        }
-        row[3] = row[3].slice(0, row[3].length - 1);
-      }
-      return row;
     }
   
 }
